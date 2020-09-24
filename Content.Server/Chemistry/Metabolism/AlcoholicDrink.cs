c07d83127eb165780d8f1c3e8daadba0627e34ff
Exp <theexp111@gmail.com>
2020-09-24 13:31:23 +0200

    Alcoholic drinks now first get broken down into ethanol

--- Content.Server/Chemistry/Metabolism/AlcoholicDrink.cs
+++ Content.Server/Chemistry/Metabolism/AlcoholicDrink.cs
@@ -1,4 +1,5 @@
-﻿using Content.Server.GameObjects.Components.Nutrition;
+﻿using Content.Server.GameObjects.Components.Body.Circulatory;
+using Content.Server.GameObjects.Components.Nutrition;
 using Content.Shared.Chemistry;
 using Content.Shared.Interfaces.Chemistry;
 using Robust.Shared.Interfaces.GameObjects;
@@ -10,36 +11,32 @@ using System.Text;
 
 namespace Content.Server.Chemistry.Metabolism
 {
-    class AlcoholicDrink : IMetabolizable
+    public class AlcoholicDrink : IMetabolizable
     {
         //Rate of metabolism in units / second
         private ReagentUnit _metabolismRate;
         public ReagentUnit MetabolismRate => _metabolismRate;
 
-        //How much thirst is satiated when 1u of the reagent is metabolized
-        private float _hydrationFactor;
-        public float HydrationFactor => _hydrationFactor;
-
-        private float _alcoholFactor;
-        public float AlhocolFactor => _alcoholFactor;
+        // How much Ethanol should be given per unit of this reagent
+        private float _ethanolFactor;
+        public float EthanolFactor => _ethanolFactor;
 
         void IExposeData.ExposeData(ObjectSerializer serializer)
         {
             serializer.DataField(ref _metabolismRate, "rate", ReagentUnit.New(1));
-            serializer.DataField(ref _hydrationFactor, "nutrimentFactor", 30.0f);
-            serializer.DataField(ref _alcoholFactor, "alcoholFactor", 1.0f);
+            serializer.DataField(ref _ethanolFactor, "ethanolFactor", 1.0f);
         }
 
-        //Remove reagent at set rate, satiate thirst if a ThirstComponent can be found
+        // Transform the drink into ethanol
         ReagentUnit IMetabolizable.Metabolize(IEntity solutionEntity, string reagentId, float tickTime)
         {
             var metabolismAmount = MetabolismRate * tickTime;
-            if (solutionEntity.TryGetComponent(out ThirstComponent thirst))
-                thirst.UpdateThirst(metabolismAmount.Float() * HydrationFactor);
-            if (solutionEntity.TryGetComponent(out DrunkComponent drunk))
-                drunk.UpdateDrunk(metabolismAmount.Float() * AlhocolFactor);
+            if (solutionEntity.TryGetComponent(out BloodstreamComponent bloodstream))
+            {
+                if (!bloodstream.Solution.TryAddReagent("chem.Ethanol", metabolismAmount * EthanolFactor, out _))
+                    return ReagentUnit.Zero;
+            }
 
-            //Return amount of reagent to be removed
             return metabolismAmount;
         }
     }
---
+++ Content.Server/Chemistry/Metabolism/Ethanol.cs
@@ -0,0 +1,47 @@
+﻿using Content.Server.GameObjects.Components.Nutrition;
+using Content.Shared.Chemistry;
+using Content.Shared.Interfaces.Chemistry;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Interfaces.Serialization;
+using Robust.Shared.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Content.Server.Chemistry.Metabolism
+{
+    public class Ethanol : IMetabolizable
+    {
+        //Rate of metabolism in units / second
+        private ReagentUnit _metabolismRate;
+        public ReagentUnit MetabolismRate => _metabolismRate;
+
+        //How much thirst is satiated when 1u of the reagent is metabolized
+        private float _hydrationFactor;
+        public float HydrationFactor => _hydrationFactor;
+
+        private float _alcoholFactor;
+        public float AlhocolFactor => _alcoholFactor;
+
+        void IExposeData.ExposeData(ObjectSerializer serializer)
+        {
+            serializer.DataField(ref _metabolismRate, "rate", ReagentUnit.New(1));
+            serializer.DataField(ref _hydrationFactor, "nutrimentFactor", 30.0f);
+            serializer.DataField(ref _alcoholFactor, "alcoholFactor", 1.0f);
+        }
+
+        //Remove reagent at set rate, satiate thirst if a ThirstComponent can be found
+        ReagentUnit IMetabolizable.Metabolize(IEntity solutionEntity, string reagentId, float tickTime)
+        {
+            var metabolismAmount = MetabolismRate * tickTime;
+            if (solutionEntity.TryGetComponent(out ThirstComponent thirst))
+                thirst.UpdateThirst(metabolismAmount.Float() * HydrationFactor);
+            if (solutionEntity.TryGetComponent(out DrunkComponent drunk))
+                drunk.UpdateDrunk(metabolismAmount.Float() * AlhocolFactor);
+
+
+            //Return amount of reagent to be removed
+            return metabolismAmount;
+        }
+    }
+}
